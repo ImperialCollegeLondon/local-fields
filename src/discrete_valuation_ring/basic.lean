18ae1205b3fddd43b7447c6f090940f07cb2c52b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-02-13 21:34:42 +0000

    yet another definition

--- src/discrete_valuation_ring/basic.lean
+++ src/discrete_valuation_ring/basic.lean
@@ -8,6 +8,4 @@ set_option old_structure_cmd true
 
 class discrete_valuation_ring (R : Type*) extends principal_ideal_domain R,
   local_ring R :=
-(pseudouniformiser : ∃ a : R, a ≠ 0 ∧ ¬ is_unit a)
-
-#check local_ring
+(exists_nonzero_nonunit : ∃ a : R, a ≠ 0 ∧ ¬ is_unit a)
